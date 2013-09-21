9e5c6e8a4ab46b1d187e138dfbfa30c07faee61a
Harin Sanghirun <harin.sanghirun@gmail.com>
2013-09-21 14:22:44 +0700

    new rails with gemfile ready

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThatDriver::Application.config.secret_key_base = '446e287888b4c93d06c49713fb178a1aaf54e4f6f6c9548ab607907062915d2c7b5cbc2b09a29b31d9b17459705b9acdea3a54ffd4c7d8d543ff25245b5c6eca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThatDriver::Application.config.session_store :cookie_store, key: '_ThatDriver_session'
